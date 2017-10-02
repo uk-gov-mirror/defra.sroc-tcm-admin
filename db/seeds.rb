30d1e67139b471b98a0df2d0349d1eea8d85a23d
Tony Headford <tony@binarycircus.com>
2017-10-02 10:56:23 +0100

    Added Regime seeds and fixed menu

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Regime.count.zero?
+  Regime.create!(name: 'PAS')
+  Regime.create!(name: 'CFD')
+  Regime.create!(name: 'WaBS')
+end
+
