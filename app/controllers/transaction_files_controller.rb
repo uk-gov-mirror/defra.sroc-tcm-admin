6bd60b9880e6c058fdbba66f73a1127f40c9f50c
Tony Headford <tony@binarycircus.com>
2017-12-06 10:43:22 +0000

    Minor refactor

--- app/controllers/transaction_files_controller.rb
+++ app/controllers/transaction_files_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class TransactionFilesController < ApplicationController
+  include RegimeScope
   before_action :set_regime, only: [:index, :create]
-  before_action :set_transaction_file, only: [:show, :edit, :update]
 
   # GET /regimes/:regime_id/transaction_files
   # GET /regimes/:regime_id/transaction_files.json
@@ -40,37 +40,12 @@ class TransactionFilesController < ApplicationController
 
   private
     # :nocov:
-    # Use callbacks to share common setup or constraints between actions.
-    def set_regime
-      # TODO: this is just to avoid not having a regime set on entry
-      # this will be replaced by using user regimes roles/permissions
-      if params.fetch(:regime_id, nil)
-        @regime = Regime.find_by!(slug: params[:regime_id])
-      else
-        @regime = Regime.first
-      end
-    end
-
     def set_region
       # TODO: this could be defaulted to a user's region if there are
       # restrictions around this
       @region = params.fetch(:region, '')
     end
     # :nocov:
-
-    def set_transaction_file
-      set_regime
-      @transaction_file = transaction_file_store.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def transaction_file_params
-      params.require(:transaction_file).permit(:category)
-    end
-
-    def collate_summary
-      transaction_store.transactions_to_be_billed_summary('', @region)
-    end
 
     def transaction_store
       @transaction_store ||= TransactionStorageService.new(@regime)
