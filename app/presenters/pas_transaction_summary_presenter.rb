022afa51c6b3a298f39e9f86e1eadd1733245251
Tony Headford <tony@binarycircus.com>
2017-12-08 14:09:40 +0000

    Updated summary query for PAS

--- app/presenters/pas_transaction_summary_presenter.rb
+++ app/presenters/pas_transaction_summary_presenter.rb
@@ -2,9 +2,17 @@
 
 class PasTransactionSummaryPresenter < TransactionSummaryPresenter
   def regime_specific_group_filter(base_query)
-    incomplete_records = base_query.without_charge.distinct.pluck(:reference_1)
-    base_query.where.not(reference_1: incomplete_records).
-      where.not(reference_2: incomplete_records).
-      where.not(reference_3: incomplete_records)
+    incomplete_records = base_query.without_charge.distinct.pluck(:reference_1, :reference_2, :reference_3)
+    ref_1_records = []
+    ref_2_records = []
+    ref_3_records = []
+    incomplete_records.each do |arr|
+      ref_1_records << arr[0] unless arr[0].nil?
+      ref_2_records << arr[1] unless arr[1].nil?
+      ref_3_records << arr[2] unless arr[2].nil?
+    end
+    base_query.where.not(reference_1: ref_1_records).
+      where.not(reference_2: ref_2_records).
+      where.not(reference_3: ref_3_records)
   end
 end
