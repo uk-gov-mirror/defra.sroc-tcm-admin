ba32a195149b7f0752f9f3228b9f8eba65a35a2b
Tony Headford <tony@binarycircus.com>
2018-02-09 15:33:55 +0000

    Added lock for import job

---
+++ db/migrate/20180209143456_create_system_configs.rb
@@ -0,0 +1,10 @@
+class CreateSystemConfigs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :system_configs do |t|
+      t.boolean :importing, null: false, default: false
+      t.datetime :import_started_at
+
+      t.timestamps
+    end
+  end
+end
