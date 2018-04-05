ca50dec02749a6613bf140adef5ffdb75d97c6f1
Tony Headford <tony@binarycircus.com>
2018-04-05 11:31:28 +0100

    Added passing error message for PAS transactions

--- app/presenters/pas_transaction_detail_presenter.rb
+++ app/presenters/pas_transaction_detail_presenter.rb
@@ -53,7 +53,8 @@ class PasTransactionDetailPresenter < TransactionDetailPresenter
       region: region_from_ref,
       period: period,
       line_amount: original_charge,
-      amount: amount
+      amount: amount,
+      error_message: error_message
     }
   end
 end
