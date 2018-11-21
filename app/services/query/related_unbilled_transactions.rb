ab54ff7fdf1a53ff038ba6c9416e3aab8569d507
Tony Headford <tony@binarycircus.com>
2018-11-21 10:14:21 +0000

    Updated related rules to ignore customer_reference

--- app/services/query/related_unbilled_transactions.rb
+++ app/services/query/related_unbilled_transactions.rb
@@ -11,21 +11,15 @@ module Query
       if regime.installations?
         q = q.where.not(reference_3: nil).
           where.not(reference_3: 'NA').
-          where(customer_reference: @transaction.customer_reference, 
-                reference_3: @transaction.reference_3).
+          where(reference_3: @transaction.reference_3).
           or(q.where.not(reference_1: 'NA').
              where.not(reference_1: nil).
-             where(customer_reference: @transaction.customer_reference,
-                   reference_1: @transaction.reference_1)).
+             where(reference_1: @transaction.reference_1)).
           or(q.where.not(reference_2: 'NA').
              where.not(reference_2: nil).
-             where(customer_reference: @transaction.customer_reference,
-                   reference_2: @transaction.reference_2))
+             where(reference_2: @transaction.reference_2))
       else
-        q = q.where.not(reference_1: nil).
-          where.not(reference_1: 'NA').
-          where(customer_reference: @transaction.customer_reference,
-                reference_1: @transaction.reference_1)
+        q = q.where(reference_1: @transaction.reference_1)
       end
       q.order(:reference_1)
     end
