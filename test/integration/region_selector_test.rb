ecba1514a74c2357e0f1ea3310b45d63c0294b58
Tony Headford <tony@binarycircus.com>
2018-11-28 13:56:37 +0000

    Added tests for region selector

--- test/integration/region_selector_test.rb
+++ test/integration/region_selector_test.rb
@@ -49,14 +49,6 @@ class RegionSelectorTest < ActionDispatch::IntegrationTest
         body.assert_selector("tr", count: row_count)
       end
     end
-    # page.select('All', from: 'region')
-    # page.find("#region>option[value='A']").click
-    # row_count = @regime.transaction_details.unbilled.count
-    # assert row_count.positive?
-    # page.find(".tcm-table>table>tbody") do |body|
-    #   body.assert_selector("tr", count: row_count)
-    # end
-    # page.assert_selector(".tcm-table>table>tbody>tr", count: row_count)
   end
 
   def test_can_see_region_selector_on_retrospective_with_all_option
@@ -64,8 +56,70 @@ class RegionSelectorTest < ActionDispatch::IntegrationTest
     assert page.has_select? "region", options: @regions_with_all
   end
 
+  def test_can_select_all_region_option_on_retrospective_view
+    visit regime_retrospectives_path(@regime)
+
+    # select a region
+    @regions_only.each do |r|
+      page.select(r, from: 'region')
+      row_count = @regime.transaction_details.region(r).retrospective.count
+      page.find(".tcm-table") do |t|
+        # does region select have correct region selected?
+        assert t.has_select?('region', selected: r),
+          "'#{r}' option not selected"
+        t.find("table>tbody") do |body|
+          body.assert_selector("tr", count: row_count)
+        end
+      end
+    end
+
+    # select all regions
+    page.select('All', from: 'region')
+    row_count = @regime.transaction_details.retrospective.count
+    page.find(".tcm-table") do |t|
+      # does region select have 'All' selected?
+      assert t.has_select?('region', selected: 'All'),
+        "All option not selected"
+
+      t.find("table>tbody") do |body|
+        body.assert_selector("tr", count: row_count)
+      end
+    end
+  end
+
   def test_can_see_region_selector_on_exclusions_with_all_option
     visit regime_exclusions_path(@regime)
     assert page.has_select? "region", options: @regions_with_all
+  end
+
+  def test_can_select_all_region_option_on_exclusions_view
+    visit regime_exclusions_path(@regime)
+
+    # select a region
+    @regions_only.each do |r|
+      page.select(r, from: 'region')
+      row_count = @regime.transaction_details.region(r).historic_excluded.count
+      page.find(".tcm-table") do |t|
+        # does region select have correct region selected?
+        assert t.has_select?('region', selected: r),
+          "'#{r}' option not selected"
+        t.find("table>tbody") do |body|
+          body.assert_selector("tr", count: row_count)
+        end
+      end
+    end
+
+    # select all regions
+    page.select('All', from: 'region')
+    row_count = @regime.transaction_details.historic_excluded.count
+    page.find(".tcm-table") do |t|
+      # does region select have 'All' selected?
+      assert t.has_select?('region', selected: 'All'),
+        "All option not selected"
+
+      t.find("table>tbody") do |body|
+        body.assert_selector("tr", count: row_count)
+      end
+    end
   end
 end
