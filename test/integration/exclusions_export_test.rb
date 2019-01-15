edb2db9879dbe41958d34bd654d084acc3a8e30c
Tony Headford <tony@binarycircus.com>
2019-01-15 11:32:07 +0000

    Added integration test for export button

---
+++ test/integration/exclusions_export_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class ExclusionsExportTest < ActionDispatch::IntegrationTest
+  include RegimeSetup
+
+  def setup
+    Capybara.current_driver = Capybara.javascript_driver
+    setup_cfd
+  end
+
+  def test_view_should_have_export_button
+    visit regime_exclusions_path(@regime)
+    assert page.has_selector? "button.table-export-btn"
+  end
+
+  def test_export_button_should_display_data_protection_notice
+    visit regime_exclusions_path(@regime)
+    page.click_button "Export matching entries"
+    page.find("#data-protection-dialog", visible: true) do |dlg|
+      assert dlg.has_text? "Data Protection Notice"
+    end
+  end
+end
+
+
+
