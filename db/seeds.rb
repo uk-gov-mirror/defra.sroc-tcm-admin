c3448eceaaae3c3cc4c14728e18f28a4bd5be494
Stuart Adair <stu@silverka.co.uk>
2020-07-07 18:07:20 +0100

    Re-order seed data to avoid errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,6 +44,36 @@ end
 #   end
 # end
 
+if User.count.zero?
+  u = User.new(first_name: 'Tony',
+               last_name: 'Headford',
+               email: 'tony@binarycircus.com',
+               role: 'admin',
+               password: "Ab0#{Devise.friendly_token.first(8)}")
+  u.regime_users.build(regime_id: Regime.first.id, enabled: true) 
+  u.save!
+end
+
+unless User.where(email: 'stu@silverka.co.uk').exists?
+  u = User.new(first_name: 'Stuart',
+                last_name: 'Adair',
+                email: 'stu@silverka.co.uk',
+                role: 'admin',
+                password: "Ab0#{Devise.friendly_token.first(8)}")
+  u.regime_users.build(regime_id: Regime.first.id, enabled: true) 
+  u.save!
+end
+
+unless User.where(email: 'system@example.com').exists?
+  u = User.new(first_name: 'System',
+               last_name: 'Account',
+               email: 'system@example.com',
+               role: 'admin',
+               password: "Ab0#{Devise.friendly_token.first(8)}")
+  u.regime_users.build(regime_id: Regime.first.id, enabled: true) 
+  u.save!
+end
+
 r = Regime.find_by!(slug: 'pas')
 r.permit_categories.destroy_all
 PermitCategoryImporter.import(r, Rails.root.join('db', 'categories', 'installations.csv'))
@@ -93,36 +123,6 @@ PermitCategoryImporter.import(r, Rails.root.join('db', 'categories', 'waste.csv'
 
 %w[ A B E N S T U Y ].each do |region|
   SequenceCounter.find_or_create_by(regime_id: r.id, region: region)
-end
-
-if User.count.zero?
-  u = User.new(first_name: 'Tony',
-               last_name: 'Headford',
-               email: 'tony@binarycircus.com',
-               role: 'admin',
-               password: "Ab0#{Devise.friendly_token.first(8)}")
-  u.regime_users.build(regime_id: Regime.first.id, enabled: true) 
-  u.save!
-end
-
-unless User.where(email: 'stu@silverka.co.uk').exists?
-  u = User.new(first_name: 'Stuart',
-                last_name: 'Adair',
-                email: 'stu@silverka.co.uk',
-                role: 'admin',
-                password: "Ab0#{Devise.friendly_token.first(8)}")
-  u.regime_users.build(regime_id: Regime.first.id, enabled: true) 
-  u.save!
-end
-
-unless User.where(email: 'system@example.com').exists?
-  u = User.new(first_name: 'System',
-               last_name: 'Account',
-               email: 'system@example.com',
-               role: 'admin',
-               password: "Ab0#{Devise.friendly_token.first(8)}")
-  u.regime_users.build(regime_id: Regime.first.id, enabled: true) 
-  u.save!
 end
 
 # add region to all transactions
