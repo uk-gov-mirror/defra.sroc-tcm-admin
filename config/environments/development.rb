ce32e94eb8b2d0780455adaf665b51268ed8a43d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-08-14 00:08:23 +0100

    Update email config for development env (#198)
    The current configuration relies on [letter_opener](https://github.com/ryanb/letter_opener) to see emails when working in `development` mode.
    
    That's fine if you are running the app directly on the host. But we also want to support the option to run the app in tools like [vagrant](https://www.vagrantup.com/) and [docker](https://www.docker.com/) where this no longer works.
    
    So this updates the config to assume you have [Mailcatcher](https://mailcatcher.me/) running when working locally. This better represents production and gives us more flexibility on where we can run the app.
    
    N.B. This config and approach are taken from Waste Carriers, Waste Exemptions and Flood Risk Activity Exemptions which are also Rails-based services.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,10 +16,6 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  # show error pages
-  # config.consider_all_requests_local = false
-  # config.action_dispatch.show_exceptions = true
-
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -34,16 +30,31 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Sending e-mails is required for confirmation emails
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("DEFAULT_URL_HOST", "http://localhost:3000"),
+    protocol: "http"
+  }
+
+  # Don't care if the mailer can't send (if set to false)
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+
+  # Default settings are for mailcatcher
+  config.action_mailer.smtp_settings = {
+    user_name: ENV.fetch("EMAIL_USERNAME", ""),
+    password: ENV.fetch("EMAIL_PASSWORD", ""),
+    domain: ENV.fetch("DEFAULT_URL_HOST", "http://localhost:3000"),
+    address: ENV.fetch("EMAIL_HOST", "localhost"),
+    port: ENV.fetch("EMAIL_PORT", 1025),
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :resque
   config.active_job.queue_name_prefix = "sroc-tcm-admin_#{Rails.env}"
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
